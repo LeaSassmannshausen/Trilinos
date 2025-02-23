2ca75513d41d2e4bfa7eaa803b085dee53892890
Lea <lea.sassmannshausen@hotmail.de>
2025-02-23 21:24:33 +0100

    adding print to extrac local bla

--- packages/shylu/shylu_dd/frosch/src/SchwarzOperators/FROSch_AlgebraicOverlappingOperator_def.hpp
+++ packages/shylu/shylu_dd/frosch/src/SchwarzOperators/FROSch_AlgebraicOverlappingOperator_def.hpp
@@ -262,6 +262,9 @@ namespace FROSch {
         FROSCH_DETAILTIMER_START_LEVELID(updateLocalOverlappingMatricesTime,"AlgebraicOverlappingOperator::updateLocalOverlappingMatrices");
         if (this->ExtractLocalSubdomainMatrix_Symbolic_Done_) {
             // using original K_ as input
+            RCP<FancyOStream> fancy = fancyOStream(rcpFromRef(cout)); 
+            this->K_->describe(*fancy,VERB_EXTREME);
+            this->subdomainMatrix_->describe(*fancy,VERB_EXTREME);
             ExtractLocalSubdomainMatrix_Compute(this->subdomainScatter_, this->K_, this->subdomainMatrix_, this->localSubdomainMatrix_);
             this->OverlappingMatrix_ = this->localSubdomainMatrix_.getConst();
         } else {
