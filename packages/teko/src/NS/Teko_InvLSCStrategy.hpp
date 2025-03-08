b76aaf22f6be46e3002e6dfc0d3a43e791fed693
Lea <lea.sassmannshausen@hotmail.de>
2025-03-08 17:53:17 +0100

    Added w scaling for LSC

--- packages/teko/src/NS/Teko_InvLSCStrategy.hpp
+++ packages/teko/src/NS/Teko_InvLSCStrategy.hpp
@@ -193,7 +193,7 @@ class InvLSCStrategy : public LSCStrategy {
   /** Set the \f$W\f$-Scaling vector used in \f$B H B^T\f$. This method
    * takes a vector.
    */
-  virtual void setWScaling(const MultiVector &wScaling) { wScaling_ = wScaling; std::cout << " Set wScaling "<< std::endl; }
+  virtual void setWScaling(const MultiVector &wScaling) { wScaling_ = wScaling; }
 
  protected:
   LinearOp massMatrix_;
